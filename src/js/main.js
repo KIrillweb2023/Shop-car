3bd28f765f008594a68f0ce6b16ed94a7c73df13
unknown <webrazrabotka72@gmail.com>
2023-12-05 19:42:15 +0300

    catalog-page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 "use strict"
 
 document.addEventListener("DOMContentLoaded", () =>{
-  console.log(1);
+
+  const cards = document.querySelectorAll(".catalog__wrapper__item");
+
+  function cardHover(cards){
+    cards.forEach((item) =>{
+      item.addEventListener("mousemove", (e) =>{
+        item.classList.add('active');
+      })
+      item.addEventListener("mouseout", (e) =>{
+        item.classList.remove('active');
+      })
+    });
+  }
+
+  cardHover(cards);
 });
