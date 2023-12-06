7a810514b62ec59d0b9bba7a144bbb345bfc7133
unknown <webrazrabotka72@gmail.com>
2023-12-06 21:03:20 +0300

    Registration-page

--- src/js/main.js
+++ src/js/main.js
@@ -16,4 +16,20 @@ document.addEventListener("DOMContentLoaded", () =>{
   }
 
   cardHover(cards);
+  const logbtn = document.querySelector(".logout__wrapper__form_login");
+  const modalLog = document.querySelector(".logout__wrapper__login");
+  const regBtn = document.querySelector(".logout__wrapper__login_login");
+  const modalReg = document.querySelector(".logout__wrapper__form");
+  function userNow(log, reg, modalReg, modalLog){
+    reg.addEventListener('click', () =>{
+      modalReg.style.display = 'none';
+      modalLog.style.display = 'flex';
+    });
+    log.addEventListener('click', () =>{
+      modalReg.style.display = '';
+      modalLog.style.display = '';
+    });
+  }
+
+  userNow(regBtn, logbtn, modalReg, modalLog);
 });
